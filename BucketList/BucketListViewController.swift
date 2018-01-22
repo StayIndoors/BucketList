e2336897c079686b6a204b2900af816544464941
Matthew Burnside <matthewsburnside@gmail.com>
2018-01-22 13:05:35 -0800

    Implemented CoreData for persistantly adding, editing, and deleting data.

--- BucketList/BucketListViewController.swift
+++ BucketList/BucketListViewController.swift
@@ -7,60 +7,103 @@
 //
 
 import UIKit
+import CoreData
 
 class BucketListViewController: UITableViewController, AddItemTableViewControllerDelegate {
     
-    var items = ["Use a Japanese bidet", "Blow up the moon", "Hunt a man"]
+    var items = [BucketListItem]()
+    
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        fetchAllItems()
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = items[indexPath.row].text!
         return cell
     }
     
     override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
         performSegue(withIdentifier: "AddItemSegue", sender: indexPath)
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let item = items[indexPath.row]
+        managedObjectContext.delete(item)
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print(error)
+        }
         items.remove(at: indexPath.row)
         tableView.reloadData()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         let navigationController = segue.destination as! UINavigationController
         let addItemTableViewController = navigationController.topViewController as! AddItemTableViewController
         addItemTableViewController.delegate = self
         if let selectedItemIP = sender as? NSIndexPath {
             let indexPath = selectedItemIP
             let item = items[indexPath.row]
-            addItemTableViewController.item = item
+            addItemTableViewController.item = item.text!
             addItemTableViewController.indexPath = indexPath
         }
     }
     
+    func fetchAllItems() {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BucketListItem")
+        do {
+            let result = try managedObjectContext.fetch(request)
+            items = result as! [BucketListItem]
+        } catch {
+            print(error)
+        }
+        
+    }
+    
     func cancelButtonPressed(by controller: AddItemTableViewController) {
+        
         dismiss(animated: true, completion: nil)
     }
 
     func itemSaved(by controller: AddItemTableViewController, with text: String, at indexPath: NSIndexPath?) {
+        
         if let ip = indexPath {
-            items[ip.row] = text
+            let item = items[ip.row]
+            item.text = text
         } else {
-            items.append(text)
+            let item  = NSEntityDescription.insertNewObject(forEntityName: "BucketListItem", into: managedObjectContext) as! BucketListItem
+            item.text = text
+            items.append(item)
         }
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print(error)
+        }
+        
         tableView.reloadData()
         dismiss(animated: true, completion: nil)
     }
